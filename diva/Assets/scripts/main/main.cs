e3c31e14bcaea7623359e597c209771070635b6b
HisayukiNakasone <h.nakasone@edia.co.jp>
2016-07-22 11:33:55 +0900

    commit

---
+++ diva/Assets/scripts/main/main.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class main : MonoBehaviour {
+
+    public Image topImage;
+    public Button startButton;
+
+    void Awake() {
+    
+    }
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void OnClickStartButton()
+    {
+
+        topImage.gameObject.SetActive(false);
+        startButton.gameObject.SetActive(false);
+    }
+
+}
