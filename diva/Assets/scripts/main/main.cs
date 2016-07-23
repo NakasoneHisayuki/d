051c55ac708b59d78754f8613c98ace7f732b45a
NakasoneHisayuki <hsyknksn@gmail.com>
2016-07-23 13:39:23 +0900

    commit

--- diva/Assets/scripts/main/main.cs
+++ diva/Assets/scripts/main/main.cs
@@ -2,58 +2,43 @@
 using UnityEngine.UI;
 using System.Collections;
 
-public class main : MonoBehaviour
-{
+public class main : MonoBehaviour{
 
     public Image topImage;
     public Button startButton;
 
     public GameObject[] globalNavi;
-    bool is_global_move = false;
+	public Animation mainContent;
+	bool is_global_move = false;
 
-    void Awake()
-    {
-        //        globalNavi[0].transform.localPosition = new Vector2(0, 115.0f);
-        //       globalNavi[1].transform.localPosition = new Vector2(-115.0f, 0);
+    void Awake(){
     }
 
     // Use this for initialization
-    void Start()
-    {
+    void Start(){
 
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update(){
 
-        if (is_global_move)
-        {
+        if (is_global_move){
             globalMove();
             is_global_move = false;
         }
     }
 
-    public void globalMove()
-    {
+    public void globalMove(){
 
-        if (globalNavi[1].transform.localPosition.x >= 0.0f)
-        {
-            iTween.Stop(globalNavi[1]);
-            globalNavi[1].transform.localPosition = new Vector2(0, 0);
-            //is_global_move = false;
-        }
-        else
-        {
-            iTween.MoveTo(globalNavi[1], iTween.Hash("x", 0.0f, "time", 20));
-        }
+		iTween.MoveTo(globalNavi[0], iTween.Hash("y", 0.0f, "time", 0.5));
+		iTween.MoveTo(globalNavi[1], iTween.Hash("x", 0.0f, "time", 0.5));
     }
 
-    public void OnClickStartButton()
-    {
+    public void OnClickStartButton(){
 
         topImage.gameObject.SetActive(false);
         startButton.gameObject.SetActive(false);
+		mainContent.Play();
         is_global_move = true;
     }
 
